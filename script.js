24c9fcee93a9a3f4540ac19a13c705a921284d14
Jamshidbek200 <jamshidbeknematjonov06@gmail.com>
2024-11-15 21:38:43 +0500

    birinchi chatgtp kutubxonasi

---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('searchBtn').addEventListener('click', function() {
+    let searchText = document.getElementById('searchInput').value;
+    alert("Qidiruv natijasi: " + searchText);
+});
